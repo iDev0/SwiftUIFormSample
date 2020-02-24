158bc31b896ae4ab2d9234804c5195211b0e87ca
SamplesMaker <insert@kakao.com>
2020-02-25 08:46:44 +0900

    SwiftUI Form Sample

--- FormSample/ContentView.swift
+++ FormSample/ContentView.swift
@@ -9,8 +9,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var airplanMode = false
+    @State var selectedMode = 0
+    
+    var appleDevices = ["iPhone", "iMac", "iPad"]
+        
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                
+                Section(header: Text("일반 셋팅")) {
+                    Toggle(isOn: $airplanMode) {
+                        Text("비행기 모드")
+                        Image(systemName: "airplane")
+                    }
+                    
+                    Picker(selection: $selectedMode, label: Text("애플 디바이스 선택")) {
+                        ForEach(0 ..< appleDevices.count) {
+                            Text(self.appleDevices[$0])
+                        }
+                    }
+                }
+                
+                Section(header: Text("더 보기")) {
+                    
+                    HStack {
+                        Text("내 디바이스")
+                        Spacer()
+                        Text("iPhone XS")
+                    }
+                    
+                    HStack {
+                        Text("소프트웨어 버전")
+                        Spacer()
+                        Text("1.11.1")
+                    }
+                }
+            }.navigationBarTitle("설정")
+        }
     }
 }
 
